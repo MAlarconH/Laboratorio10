4e548e0d5c89cb2d8fafb7b36e96f2433351759a
Alumno <Alumno@LAB707-10>
2023-05-22 21:40:06 -0500

    Agregar archivos de proyecto.

---
+++ Semana10/Controllers/productsController.cs
@@ -0,0 +1,42 @@
+﻿using Semana10.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Semana10.Controllers
+{
+    public class productsController : Controller
+    {
+        private NeptunoEntities1 db = new NeptunoEntities1();
+        // GET: products
+        public ActionResult Index()
+        {
+            return View(db.productos.ToList());
+        }
+
+        // GET: Product/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Product/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "nombreProducto,precioUnidad,categoriaProducto")] productos producto)
+        {
+            if (ModelState.IsValid)
+            {
+                db.productos.Add(producto);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(producto);
+        }
+
+
+    }
+}
